6399231efd3b99ad6e64e34b027732bad645075e
Seham Othman <210709718@posta.mu.edu.tr>
2024-10-10 13:03:55 +0300

    commit all changes and make week3Lab

--- week3lab/app/src/main/java/com/example/week3lab/MainActivity.java
+++ week3lab/app/src/main/java/com/example/week3lab/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.week3lab;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    ListView listview;
+    List<Post> postList = new ArrayList<>();
+    Button btnPost;
+    static final int POST_REQUEST = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        listview = findViewById(R.id.listView);
+        PostAdapter postAdapter = new PostAdapter(this, postList);
+        listview.setAdapter(postAdapter);
+
+        btnPost = findViewById(R.id.btnPost);
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                startActivityForResult(intent, POST_REQUEST);
+            }
         });
+
+
     }
 }
